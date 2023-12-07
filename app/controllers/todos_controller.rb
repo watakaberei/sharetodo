a3b1f4d15183ce38ff77ab80de54d16b1229773e
watakaberei <126738738+watakaberei@users.noreply.github.com>
2023-12-07 10:00:05 +0000

    newとcreateメソッドにおける定義

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,14 @@
 class TodosController < ApplicationController
+
   def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.user_id = current_user.id
+    @todo.save
+    redirect_to todo_path
   end
 
   def index
@@ -9,5 +18,12 @@ class TodosController < ApplicationController
   end
 
   def edit
+  end
+
+
+  private
+
+  def  todo_params
+    params.require(:todo).permit(:title, :body)
   end
 end
