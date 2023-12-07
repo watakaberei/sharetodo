83eab3b7caec3ac989e07a55ebf9d5890e18510d
watakaberei <126738738+watakaberei@users.noreply.github.com>
2023-12-07 10:01:04 +0000

    createの追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :todos, only: [:new, :index, :show, :edit]
+  resources :todos, only: [:new, :create, :index, :show, :edit]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
