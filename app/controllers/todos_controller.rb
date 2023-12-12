bb20fd7cefd7d7b5221d08f10f3cf06839e87c7e
watakaberei <126738738+watakaberei@users.noreply.github.com>
2023-12-12 05:21:24 +0000

    todoにおけるメソッド定義

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,8 @@
 class TodosController < ApplicationController
+  
+  def index
+    @todos = Todo.all
+  end
 
   def new
     @todo = Todo.new
@@ -8,22 +12,31 @@ class TodosController < ApplicationController
     @todo = Todo.new(todo_params)
     @todo.user_id = current_user.id
     @todo.save
-    redirect_to todo_path
-  end
-
-  def index
+    redirect_to todos_path
   end
 
   def show
   end
 
   def edit
+    @todo = Todo.find(params[:id])
+  end
+  
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update(todo_params)
+    redirect_to todos_path
   end
 
+  def destroy
+    todo = Todo.find(params[:id])
+    todo.destroy
+    redirect_to todos_path
+  end
 
   private
 
   def  todo_params
-    params.require(:todo).permit(:title, :body)
+    params.require(:todo).permit(:title)
   end
 end
