8c48638358d4bdf0d217d559429fdc7fb4b4fe79
watakaberei <126738738+watakaberei@users.noreply.github.com>
2023-12-13 01:55:43 +0000

    userにおけるrouting設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :todos, only: [:new, :create, :index, :show, :edit]
+  resources :todos, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
